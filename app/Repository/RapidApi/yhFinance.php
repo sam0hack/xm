1afbcfc66d7bafaaac8c3bcc26b1590b6bda6289
sam0hack <sam.nyx@live.com>
2023-09-10 05:24:11 +0530

    Added: start and end date and Add method to filter data based on key

--- app/Repository/RapidApi/yhFinance.php
+++ app/Repository/RapidApi/yhFinance.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository\RapidApi;
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Http;
 
 class yhFinance implements \App\Repository\RapidApi\FinanceInterface
@@ -10,19 +11,50 @@ class yhFinance implements \App\Repository\RapidApi\FinanceInterface
     protected string $url = 'https://yh-finance.p.rapidapi.com/stock/v3/get-historical-data?';
     protected string $region;
     protected string $symbol;
+    private string $start_date;
+    private string $end_date;
 
-    public function __construct(string $symbol, string $region = 'US')
+    public function __construct(string $symbol, string $start_date, string $end_date, string $region = 'US')
     {
         $this->region = $region;
         $this->symbol = $symbol;
+        $this->start_date = $start_date;
+        $this->end_date = $end_date;
     }
 
     /**
-     * @return array|mixed
+     * @return array
      */
-    function get_historical_data(): mixed
+    function get_historical_data(): array
     {
         $data = Http::withHeaders(['X-RapidAPI-Key' => env('X_RAPID_API_KEY'), 'X-RapidAPI-Host' => $this->host])->get($this->url . 'symbol=' . $this->symbol . '&region=' . $this->region);
-        return $data->json();
+        return $this->filterWithDateRange($data->json());
+
     }
+
+    protected function filterWithDateRange($data): array
+    {
+        $data = ($data['prices']);
+
+        $filtered_data = [];
+
+        $startTimestamp = strtotime($this->start_date);
+        $endTimestamp = strtotime($this->end_date);
+
+
+        for ($i = 0; $i < count($data); $i++) {
+
+            if ($data[$i]['date'] >= $startTimestamp && $data[$i]['date'] <= $endTimestamp) {
+                //Timestamp to date
+                $data[$i]['date'] = $date = date("Y-m-d", $data[$i]['date']);
+                $filtered_data[] = $data[$i];
+            }
+
+        }
+
+        return $filtered_data;
+
+    }
+
+
 }
