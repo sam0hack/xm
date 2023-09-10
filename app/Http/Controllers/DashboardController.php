0ea1ca62ab19c48c21d99006daa42400ce91f9c6
sam0hack <sam.nyx@live.com>
2023-09-10 05:32:13 +0530

    Added: Mail and fix api method

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,33 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\CompanyData;
 use App\Repository\Nasdaq\ListingsInterface;
+use App\Repository\RapidApi\RapidApiRepo;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
     private ListingsInterface $listings;
+    private RapidApiRepo $rapidApi;
 
-    public function __construct(ListingsInterface $listings)
+    public function __construct(ListingsInterface $listings, RapidApiRepo $rapidApiRepo)
     {
-        $this->listings = $listings;
+        $this->listings = $listings->listings();
+        $this->rapidApi = $rapidApiRepo;
     }
 
     public function index(): \Inertia\Response
     {
         return Inertia::render('Dashboard',[
-            'listings'=> json_encode($this->listings->listings()->getSymbol())
+            'listings'=> json_encode($this->listings->getSymbol())
         ]);
     }
 
-    public function getHistoricalData(Request $request){
-
-        //$start_date = Carbon::createFromDate($request->input('start_date'))->format('Y-m-d');
-
-        //dd($request->all());
-
+    public function getHistoricalData(Request $request)
+    {
 
         $today = now();
 
@@ -39,7 +40,18 @@ class DashboardController extends Controller
             'end_date' => "required|date|before_or_equal:$today|after_or_equal:start_date",
         ]);
 
-        return response(['done']);
+        $historical_data = $this->rapidApi->finance('yhFinance',$request->input('company_symbol'),$request->input('start_date'),$request->input('end_date'))->get_historical_data();
+
+        $company_name = $this->listings->getNameBySymbol($request->input('company_symbol'));
+
+
+        Mail::to($request->input('email'))->queue(new CompanyData($company_name,$request->input('company_symbol'),$request->input('start_date'),$request->input('end_date')));
+
+        return response($historical_data);
+//        return Inertia::render('Dashboard',[
+//            'historical_data'=> json_encode($historical_data),
+//            'listings'=> json_encode($this->listings->listings()->getSymbol())
+//        ]);
 
     }
 }
