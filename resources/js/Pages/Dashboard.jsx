cf1b1010f6883af3fac8159b5970ef5811d8b526
sam0hack <sam.nyx@live.com>
2023-09-10 05:31:39 +0530

    Clean up

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,19 +1,18 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
-import HistoricalData from "@/Pages/HistoricalData.jsx";
+import {Head} from '@inertiajs/react';
+import GetHistoricalData from "@/Pages/Partials/GetHistoricalData.jsx";
 
 
-export default function Dashboard({ auth , listings}) {
+export default function Dashboard({auth, listings, historical_data}) {
+
+
     return (
         <AuthenticatedLayout
             user={auth.user}
-
         >
-            <Head title="Dashboard" />
+            <Head title="Dashboard"/>
 
-            <HistoricalData listings={listings}/>
-
-
+            <GetHistoricalData listings={listings}/>
 
         </AuthenticatedLayout>
     );
