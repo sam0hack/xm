fae2256497db7cf4ede965f56994d2351fc330cb
sam0hack <sam.nyx@live.com>
2023-09-10 05:34:15 +0530

    Fix: Show data

--- resources/js/Pages/Partials/HistoricalDataTable.jsx
+++ resources/js/Pages/Partials/HistoricalDataTable.jsx
@@ -1,10 +1,11 @@
-export default function HistoricalDataTable({historicalData}) {
+export default function HistoricalDataTable({historical_data}) {
 
     return (
         <section className=''>
             <header>
                 <h2 className="text-lg font-medium text-gray-900 dark:text-gray-100">Historical Data</h2>
             </header>
+
 
 
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
@@ -29,37 +30,34 @@ export default function HistoricalDataTable({historicalData}) {
                         <th scope="col" className="px-6 py-3">
                             Volume
                         </th>
-                        <th scope="col" className="px-6 py-3">
-                            <span className="sr-only">View Chart</span>
-                        </th>
+
                     </tr>
                     </thead>
                     <tbody>
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                        <th scope="row"
-                            className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Apple MacBook Pro 17"
-                        </th>
-                        <td className="px-6 py-4">
-                            Silver
-                        </td>
-                        <td className="px-6 py-4">
-                            Laptop
-                        </td>
-                        <td className="px-6 py-4">
-                            $2999
-                        </td>
-                        <td className="px-6 py-4">
-                            $2999
-                        </td>
-                        <td className="px-6 py-4">
-                            $2999
-                        </td>
-                        <td className="px-6 py-4 text-right">
-                            <a href="#"
-                               className="font-medium text-blue-600 dark:text-blue-500 hover:underline">View Chart</a>
-                        </td>
-                    </tr>
+
+                        {historical_data.map((data, index) => (
+                            <tr className="bg-white border-b " key={index}>
+                            <td className="px-6 py-4" >{data.date}</td>
+                            <td className="px-6 py-4">
+                                {data.open}
+                            </td>
+                            <td className="px-6 py-4">
+                                {data.high}
+                            </td>
+
+                                <td className="px-6 py-4">
+                                    {data.low}
+                                </td>
+
+                                <td className="px-6 py-4">
+                                    {data.close}
+                                </td>
+                                <td className="px-6 py-4">
+                                    {data.volume}
+                                </td>
+
+                            </tr>
+                        ))}
 
                     </tbody>
                 </table>
